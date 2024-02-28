13011fdce17f92437d2ca12dd910215832717251
romawcka <yakubovroma5@gmail.com>
2024-02-28 16:03:16 +0100

    - components were implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,19 @@
+import { Container } from 'react-bootstrap';
+import { Footer, Header } from './components/index';
+import { Home } from './screen';
+
 const App = () => {
-  return <div>Component</div>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Home />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
