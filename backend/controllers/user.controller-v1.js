2295868a5a176bea7823e065669ed66a95ca99c0
romawcka <yakubovroma5@gmail.com>
2024-03-10 11:18:12 +0100

    - user fn controllers

--- backend/controllers/user.controller-v1.js
+++ backend/controllers/user.controller-v1.js
@@ -1,6 +1,6 @@
+import jwt from 'jsonwebtoken';
 import { asyncHandler } from '../middleware/asyncHandler.js';
 import { User } from '../models/user.model.js';
-import { generateToken } from '../utils/generateToken.js';
 
 // @desc -> fetch aurh user + get token
 // @ route -> POST 'api/users/login'
@@ -11,7 +11,17 @@ const loginUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
-    generateToken(res, user._id);
+    const token = jwt.sign({ userID: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '30d',
+    });
+
+    //@ Set jwt as http-only cookie
+    res.cookie('jwt', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== 'development',
+      sameSite: 'strict',
+      maxAge: 30 * 23 * 60 * 60 * 1000, // 30 Days
+    });
 
     res.json({
       _id: user._id,
@@ -46,8 +56,17 @@ const registerUser = asyncHandler(async (req, res) => {
 
   // token creation
   if (user) {
-    generateToken(res, user._id);
+    const token = jwt.sign({ userID: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '30d',
+    });
 
+    //@ Set jwt as http-only cookie
+    res.cookie('jwt', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== 'development',
+      sameSite: 'strict',
+      maxAge: 30 * 23 * 60 * 60 * 1000, // 30 Days
+    });
     res.status(201).json({
       _id: user._id,
       name: user.name,
@@ -76,48 +95,14 @@ const logoutUser = asyncHandler(async (req, res) => {
 // @route -> GET 'api/users/profile'
 // @access -> private
 const getUserProfile = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.user._id);
-
-  if (user) {
-    res.status(200).json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-    });
-  } else {
-    res.status(404);
-    throw new Error('User not found');
-  }
+  res.send('get user profile');
 });
 
 // @desc -> update user profile
 // @route -> PUT 'api/users/profile'
 // @access -> private
 const updateUserProfile = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.user._id);
-
-  if (user) {
-    user.name = req.body.name || user.name; // для обновление user name будет использовано либо то что будет в body.name или то, что уже есть
-    user.email = req.body.email || user.email;
-
-    if (req.body.password) {
-      user.password = req.body.password; // нужно проверять, есть ли что-то в поле password для обновления, тк у нас захеширован пароль в базе данных, и его просто так не получится использовать как user.name / email
-    }
-
-    const updatedUser = await user.save();
-
-    res.status(200).json({
-      _id: updatedUser._id,
-      name: updatedUser.name,
-      email: updatedUser.email,
-      isAdmin: updatedUser.isAdmin,
-    });
-  } else {
-    res.status(404).json({
-      message: 'User not found',
-    });
-  }
+  res.send('update user profile');
 });
 
 // @desc -> get list of users/users
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -31,4 +31,12 @@ UserSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 export const User = mongoose.model('User', UserSchema);
