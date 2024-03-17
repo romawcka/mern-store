7704cfb3799cdeacae70a7ca38e77976c6a65436
romawcka <yakubovroma5@gmail.com>
2024-03-17 15:00:45 +0100

    📍order routes + controllers created;

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,34 @@
 import cookieParser from 'cookie-parser';
-import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import { mongooseConnection } from './db.js';
 import { errorHandler } from './middleware/errorHandler.js';
+import { router as orderRoutes } from './routes/order.route.js';
 import { router as productRoutes } from './routes/products.route.js';
 import { router as usersRoutes } from './routes/users.route.js';
 dotenv.config();
 
 // port where app will work
+// eslint-disable-next-line no-undef
 const port = process.env.PORT;
 
 // initialization of app
 const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  next();
+});
 
 // body parser (will able to read data from req.body)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // cookie parser (will able to react data from cookie)
 app.use(cookieParser());
-// unable cros policy
-// app.use(cors());
 
 // launching app
 app.get('/', (req, res) => {
@@ -29,6 +37,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', usersRoutes);
+app.use('/api/orders', orderRoutes);
 
 // error handlersapp.use(notFound);
 app.use(errorHandler);
@@ -36,4 +45,8 @@ app.use(errorHandler);
 mongooseConnection();
 
 // live-server
-app.listen(port, () => console.log(`Server is running on port ${port}. CORS-enabled web server listening on ${port}`));
+app.listen(port, () =>
+  console.log(
+    `Server is running on port ${port}. CORS-enabled web server listening on ${port}`,
+  ),
+);
---
+++ backend/routes/order.route.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import { admin, protect } from '../middleware/authMiddlleware.js';
+import {
+  addOrderItems,
+  getMyOrders,
+  getOrder,
+  getOrders,
+  updateOrderToDelivered,
+  updateOrderToPaid,
+} from '../controllers/order.controller.js';
+
+export const router = express.Router();
+
+// /api/orders
+// @ -> private route | -> admin route
+router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders);
+// @ -> private route --> /api/orders/myorders
+router.get('/myorders', protect, getMyOrders);
+// @ -> admin route --> /api/orders/:id
+router.get('/:id', protect, admin, getOrder);
+// @ -> private route --> /api/orders/:paid
+router.put('/:id/paid', protect, updateOrderToPaid);
+// @ -> admin route --> /api/orders/:delivered
+router.put('/:id/delivered', protect, admin, updateOrderToDelivered);
