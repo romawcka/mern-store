601cadb4ba4a4521d62c682d3048f8f458b1b2fe
romawcka <yakubovroma5@gmail.com>
2024-04-04 18:51:24 +0200

    📍user deletion

--- frontend/src/screen/Admin/UserList/index.jsx
+++ frontend/src/screen/Admin/UserList/index.jsx
@@ -1,15 +1,39 @@
+import { toast } from 'react-toastify';
 import { CustomizedTable, Loader, Message } from '../../../components';
-import { useGetUsersQuery } from '../../../slices/usersApiSlice';
+import {
+  useDeleteUserMutation,
+  useGetUsersQuery,
+} from '../../../slices/usersApiSlice';
 
 const UserList = () => {
-  const { data: users, isLoading, error, refetch } = useGetUsersQuery();
+  const {
+    data: users,
+    isLoading,
+    error: usersError,
+    refetch,
+  } = useGetUsersQuery();
 
-  const handleDelete = (id) => console.log(`delete ${id}`);
+  const [deleteUser, { isLoading: isDeleting, error: deleteError }] =
+    useDeleteUserMutation();
+
+  const handleDelete = async (id) => {
+    if (window.confirm('Would you like to delete user?'))
+      try {
+        await deleteUser(id);
+        toast.success('User was successfully deleted');
+        refetch();
+      } catch (error) {
+        toast.error(error?.data?.message || error.error);
+      }
+  };
+
+  const loading = isDeleting || isLoading;
+  const error = usersError || deleteError;
 
   return (
     <>
       <h1>Users</h1>
-      {isLoading && <Loader />}
+      {loading && <Loader />}
       {error && <Message>{error}</Message>}
       <CustomizedTable datum={users} type="users" handleDelete={handleDelete} />
     </>
